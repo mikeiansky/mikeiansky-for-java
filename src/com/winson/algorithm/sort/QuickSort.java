faec7916260bbd301492d17a9dab089e714e00da
zhouwenxiang@shixiba.com <929566854@qq.com>
2020-12-01 14:08:16 +0800

    添加快速排序算法;

--- src/com/winson/algorithm/sort/QuickSort.java
+++ src/com/winson/algorithm/sort/QuickSort.java
@@ -13,7 +13,8 @@ public class QuickSort {
         System.out.println("before quick sort");
         SortUtil.printArr(arr);
 
-        quickSort(arr, 0, arr.length - 1);
+//        quickSort(arr, 0, arr.length - 1);
+        quick_sort(arr, 0, arr.length - 1);
 //        quickSort(arr, 0, arr.length - 1, 3);
 //        GoodQuickSort.quickSort(arr, arr.length);
 
@@ -48,6 +49,37 @@ public class QuickSort {
 
         quickSort(qa, p, pivot - 1);
         quickSort(qa, pivot + 1, r);
+    }
+
+
+    /*
+     * 快速排序
+     *
+     * 参数说明：
+     *     a -- 待排序的数组
+     *     l -- 数组的左边界(例如，从起始位置开始排序，则l=0)
+     *     r -- 数组的右边界(例如，排序截至到数组末尾，则r=a.length-1)
+     */
+    public static void quick_sort(int a[], int l, int r) {
+        if (l < r) {
+            int i, j, x;
+            i = l;
+            j = r;
+            x = a[i];
+            while (i < j) {
+                while (i < j && a[j] > x)
+                    j--; // 从右向左找第一个小于x的数
+                if (i < j)
+                    a[i++] = a[j];
+                while (i < j && a[i] < x)
+                    i++; // 从左向右找第一个大于x的数
+                if (i < j)
+                    a[j--] = a[i];
+            }
+            a[i] = x;
+            quick_sort(a, l, i - 1); /* 递归调用 */
+            quick_sort(a, i + 1, r); /* 递归调用 */
+        }
     }
 
 }
