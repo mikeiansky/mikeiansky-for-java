b511d81d9a15ffe23714351b49561b6f9ae7c53a
zhouwenxiang@shixiba.com <929566854@qq.com>
2020-12-24 15:55:29 +0800

    修改计算;

--- main/src/com/winson/calc/TestMath.java
+++ main/src/com/winson/calc/TestMath.java
@@ -12,17 +12,19 @@ import java.util.Date;
 public class TestMath {
 
     public static void main(String[] args) throws ParseException {
-        String tdStr = "2020-12-15 00:00:00";
-        String cdStr = "2020-12-16 00:00:00";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date targetDate = sdf.parse(tdStr);
-        Date currentDate = sdf.parse(cdStr);
-//        System.out.println(currentDate.getTime()-targetDate.getTime());
-        System.out.println(Math.pow(1.1,50));
+//        String tdStr = "2020-12-15 00:00:00";
+//        String cdStr = "2020-12-16 00:00:00";
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        Date targetDate = sdf.parse(tdStr);
+//        Date currentDate = sdf.parse(cdStr);
+////        System.out.println(currentDate.getTime()-targetDate.getTime());
+//        System.out.println(Math.pow(1.1,50));
+//
+//        double radius = Math.pow(1,2)+Math.pow(1,2);
+//        double sqrt = Math.sqrt(radius);
+//        System.out.println(radius);
 
-        double radius = Math.pow(1,2)+Math.pow(1,2);
-        double sqrt = Math.sqrt(radius);
-        System.out.println(radius);
+        System.out.println(Math.pow(1.2,-2));
 
     }
 
--- main/src/com/winson/date/TestDate.java
+++ main/src/com/winson/date/TestDate.java
@@ -1,6 +1,10 @@
 package com.winson.date;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author winson
@@ -8,17 +12,28 @@ import java.util.Calendar;
  **/
 public class TestDate {
 
-    public static void main(String[] args) {
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MONTH,-23);
-        int year = calendar.get(Calendar.YEAR);
+    public static void main(String[] args) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2020-12-24 09:00:00");
+        Instant reqInstant = date.toInstant();
+//        System.out.println("now-date:"+new Date());
+//        System.out.println("now-date:"+new Date().getTime());
+        System.out.println("date:"+new Date());
+        System.out.println("systemt-time:"+System.currentTimeMillis());
+        System.out.println("instant-time:"+reqInstant.toEpochMilli());
+//        System.out.println("instant-now:"+Instant.now());
+//        System.out.println("instant-time:"+Instant.now().toEpochMilli());
+//        System.out.println("now:"+new Date());
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.add(Calendar.MONTH,-23);
+//        int year = calendar.get(Calendar.YEAR);
 //        int month = calendar.get(Calendar.MONTH)+1;
 //        int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        System.out.println(year);
+//        System.out.println(year);
 //        System.out.println(month);
 //        System.out.println(day);
+
+
 
 
     }
