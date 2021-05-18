e3deef22bbb60e0c7c2cb3a35da99f151c18db09
zhouwenxiang@shixiba.com <929566854@qq.com>
2021-05-18 14:10:25 +0800

    测试继承类的虚拟机方法表;

--- src/main/java/com/winson/syntax/overload/TestOverLoad.java
+++ src/main/java/com/winson/syntax/overload/TestOverLoad.java
@@ -1,34 +1,63 @@
 package com.winson.syntax.overload;
 
+
 /**
  * @author winson
  * @date 2021/5/18
  **/
 public class TestOverLoad {
 
-    public static class Car{
-        public static void run(String msg){
-            System.out.println("car run... " + msg);
+    public static class Machine{
+        public void make(String msg){
+            System.out.println("machine make ... " + msg);
         }
+    }
+
+    public static class Car extends Machine{
+
+//        public static void run(String msg){
+//            System.out.println("car run... " + msg);
+//        }
+//
+//        public void move(int distance){
+//            System.out.println("car move distance : " + distance);
+//        }
+
+        public final void make(String msg){
+            System.out.println("car make ... " + msg);
+        }
+
     }
 
     public static class Benz extends Car {
-        public static void run(int speed){
-            System.out.println("benz run... " + speed);
-        }
+//        public static void run(String speed){
+//            System.out.println("benz run... " + speed);
+//        }
+//
+//        public void move(double distance){
+//            System.out.println("benz move distance : " + distance);
+//        }
+//        public void make(String msg){
+//            System.out.println("benz make ... " + msg);
+//        }
+
     }
 
     public static void main(String[] args) {
-        System.out.println("test over load start ... ");
+//        Car.run("shenzhen");
+//        Benz.run("shenzhen");
 
-        Car car = new Car();
-        car.run("shenzhen");
-        Car.run("shenzhen");
+//        Benz benz = new Benz();
+//        benz.move(1.1);
+//        benz.move(2);
+//
+//        Random random = new Random();
+//        random.nextDouble();
 
-        Benz.run("shenzhen");
-        Benz.run(20);
+        Machine machine = new Benz();
+        machine.make("my menz");
 
-        System.out.println("test over load end ... ");
+//        System.out.println("test over load end ... ");
     }
 
 }
