f8cb422e571819c2fc38e4bdcb790d7dcf7ffebb
zhouwenxiang@shixiba.com <929566854@qq.com>
2021-06-02 17:19:04 +0800

    测试reactor编程;

---
+++ src/main/java/com/winson/reactor3/TestReactor3.java
@@ -0,0 +1,55 @@
+package com.winson.reactor3;
+
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * @author winson
+ * @date 2021/6/2
+ **/
+public class TestReactor3 {
+
+    private static Flux<String> getZipDescFlux() {
+        String desc = "Zip two sources together, that is to say wait for all the sources to emit one element and combine these elements once into a Tuple2.";
+        return Flux.fromArray(desc.split("\\s+"));  // 1
+    }
+
+    public static void testSimpleOperators() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);  // 2
+        Flux.zip(
+                getZipDescFlux(),
+                Flux.interval(Duration.ofMillis(200)))  // 3
+                .subscribe(t -> System.out.println(t.getT1()), null, countDownLatch::countDown);    // 4
+        countDownLatch.await(10, TimeUnit.SECONDS);     // 5
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("test reactor3 start ... ");
+
+        testSimpleOperators();
+
+//        Flux.just(1, 2, 3, 4)
+//                .filter(new Predicate<Integer>() {
+//                    @Override
+//                    public boolean test(Integer integer) {
+//                        return integer > 2;
+//                    }
+//                }).subscribe(new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer integer) {
+//                System.out.println("accept : " + integer);
+//            }
+//        });
+
+        System.out.println("test reactor3 stop ... ");
+    }
+
+}
--- src/main/java/com/winson/rxjava/TestRxJava.java
+++ src/main/java/com/winson/rxjava/TestRxJava.java
@@ -8,8 +8,10 @@ import rx.functions.Action1;
 import rx.functions.Func0;
 import rx.functions.Func1;
 import rx.observables.SyncOnSubscribe;
+import rx.schedulers.Schedulers;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author winson
@@ -20,13 +22,33 @@ public class TestRxJava {
     public static void main(String[] args) {
         System.out.println("test rx java start ... ");
 
-        Observable.just(1, 2, 3, 4, 5, 6, 7)
-                .flatMap((Func1<Integer, Observable<Integer>>) before -> {
-                    System.out.println("call before : " + before);
-                    return Observable.just(before + 10);
-                })
-                .subscribe(new Subscriber<Integer>() {
+//        Observable.just(1, 2, 3, 4, 5, 6, 7)
+//                .flatMap((Func1<Integer, Observable<Integer>>) before -> {
+//                    System.out.println("call before : " + before);
+//                    return Observable.just(before + 10);
+//                })
+//                .subscribe(new Subscriber<Integer>() {
+//
+//                    @Override
+//                    public void onCompleted() {
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(Integer result) {
+//                        System.out.println("on next : " + result);
+//                    }
+//                });
 
+        Observable.timer(1, TimeUnit.SECONDS);
+        Observable.just(1, 2, 3, 4, 5)
+                .repeat(Schedulers.immediate())
+                .subscribe(new Observer<Integer>() {
                     @Override
                     public void onCompleted() {
 
@@ -38,8 +60,8 @@ public class TestRxJava {
                     }
 
                     @Override
-                    public void onNext(Integer result) {
-                        System.out.println("on next : " + result);
+                    public void onNext(Integer integer) {
+                        System.out.println("just on next : " + integer);
                     }
                 });
 
