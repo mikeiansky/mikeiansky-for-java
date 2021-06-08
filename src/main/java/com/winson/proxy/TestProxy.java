7ca43c28f1b37eef96ebd8bb5539a26e9e9d598e
zhouwenxiang@shixiba.com <929566854@qq.com>
2021-06-08 19:00:29 +0800

    添加测试代码;

---
+++ src/main/java/com/winson/proxy/TestProxy.java
@@ -0,0 +1,18 @@
+package com.winson.proxy;
+
+/**
+ * @author winson
+ * @date 2021/6/8
+ **/
+public class TestProxy {
+
+    public static void main(String[] args) {
+
+        System.out.println("test proxy start ... ");
+
+
+        System.out.println("test proxy stop ... ");
+
+    }
+
+}
