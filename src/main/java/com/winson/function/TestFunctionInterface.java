6030d826c7b947a41f9be9ecf832e4fdad6deead
zhouwenxiang@shixiba.com <929566854@qq.com>
2021-06-17 12:06:48 +0800

    添加Function类identity方法测试;

--- src/main/java/com/winson/function/TestFunctionInterface.java
+++ src/main/java/com/winson/function/TestFunctionInterface.java
@@ -1,6 +1,10 @@
 package com.winson.function;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author winson
@@ -8,11 +12,47 @@ import java.util.function.Function;
  **/
 public class TestFunctionInterface {
 
+    static class Person {
+        private String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("test function interface start ... ");
-        testFunction();
+//        testFunction();
+        testFunctionIdentity();
         System.out.println("test function interface end ...");
 
+    }
+
+    private static void testFunctionIdentity() {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("one"));
+        personList.add(new Person("two"));
+        personList.add(new Person("three"));
+        personList.add(new Person("four"));
+
+        Map<String, Person> personMap = personList.stream().collect(Collectors.toMap(Person::getName, Function.identity()));
+        System.out.println("personList : " + personList);
+        System.out.println("personMap : " + personMap);
     }
 
     public static void testFunction() {
@@ -50,6 +90,9 @@ public class TestFunctionInterface {
                 return "last apply : " + s;
             }
         }).apply(4));
+
+        System.out.println(Function.identity());
+        ;
 
     }
 
