5ee21bba724f6c85d30b225960c2ab8879973534
zhouwenxiang@shixiba.com <929566854@qq.com>
2021-06-18 12:23:55 +0800

    添加cglib代理类测试;

--- src/main/java/com/winson/proxy/TestProxy.java
+++ src/main/java/com/winson/proxy/TestProxy.java
@@ -8,6 +8,19 @@ import net.sf.cglib.proxy.Enhancer;
  **/
 public class TestProxy {
 
+    public interface Person {
+        void sayGood(String msg);
+    }
+
+    public static class User implements Person {
+
+        @Override
+        public void sayGood(String msg) {
+            System.out.println("user say good msg : " + msg);
+        }
+
+    }
+
     public void sayHello(String msg) {
         System.out.println("proxy say hello : " + msg);
     }
@@ -23,11 +36,22 @@ public class TestProxy {
         enhancer.setCallback(proxy);
         System.out.println("1");
         TestProxy target = (TestProxy) enhancer.create();
-        System.out.println("testProxy--->"+testProxy);
-        System.out.println("target--->"+target);
-        System.out.println("target.getClass()--->"+target.getClass());
+        System.out.println("testProxy--->" + testProxy);
+        System.out.println("target--->" + target);
+        System.out.println("target.getClass()--->" + target.getClass());
         System.out.println("ready invoke method ... ");
         target.sayHello("winson");
+        System.out.println("---------- next test ---------- ");
+
+        User user = new User();
+        CGlibProxy proxy2 = new CGlibProxy(user);
+        Enhancer e2 = new Enhancer();
+        e2.setSuperclass(Person.class);
+        e2.setCallback(proxy2);
+        Person person = (Person) e2.create();
+        System.out.println("person : " + person);
+        System.out.println("person.getClass() : " + person.getClass());
+        person.sayGood("haha");
 
         System.out.println("test proxy stop ... ");
     }
