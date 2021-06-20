62bb0fc4ddda8af14c933fa45c39d6340f60603e
winson@home <929566854@qq.com>
2021-06-20 10:23:54 +0800

    注解测试;

---
+++ src/main/java/com/winson/annotation/UserGroup.java
@@ -0,0 +1,14 @@
+package com.winson.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author winson
+ * @date 2021/6/20
+ **/
+@Documented
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Super
+public @interface UserGroup {
+}
