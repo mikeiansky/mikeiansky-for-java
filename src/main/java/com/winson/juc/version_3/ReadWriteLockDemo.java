e1887be0bd86a90b526d6d66c066071f860c3fb8
zhouwenxiang@shixiba.com <929566854@qq.com>
2021-06-21 14:10:38 +0800

    测试读写锁;

---
+++ src/main/java/com/winson/juc/version_3/ReadWriteLockDemo.java
@@ -0,0 +1,60 @@
+package com.winson.juc.version_3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Supplier;
+
+/**
+ * @author winson
+ * @date 2021/6/21
+ **/
+public class ReadWriteLockDemo {
+
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private Lock readLock = lock.readLock();
+
+    private Lock writeLock = lock.writeLock();
+
+    private Map<String, String> cache = new HashMap<>();
+
+    public String get(String key, Supplier<String> supplier) {
+        readLock.lock();
+        String result = cache.get(key);
+        readLock.unlock();
+        if (result == null) {
+            writeLock.lock();
+            result = cache.get(key);
+            if (result == null) {
+                cache.put(key, supplier.get());
+                result = cache.get(key);
+            }
+            writeLock.unlock();
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        final ReadWriteLockDemo demo = new ReadWriteLockDemo();
+        int size = 100;
+        Thread[] threads = new Thread[size];
+        for (int i = 0; i < size; i++) {
+            final int threadFlag = i;
+            threads[i] = new Thread(() -> System.out.println(demo.get("key", () -> "haha : " + threadFlag)));
+            threads[i].start();
+        }
+
+        try {
+            for (int i = 0; i < size; i++) {
+                threads[i].join();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
