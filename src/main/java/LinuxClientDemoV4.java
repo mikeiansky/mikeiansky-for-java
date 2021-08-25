3c6e662aaf34e92bee45e937b841599be2aa7a59
zhouwenxiang@shixiba.com <929566854@qq.com>
2021-08-25 18:20:16 +0800

    Socket最大连接数测试;

--- src/main/java/LinuxClientDemoV4.java
+++ src/main/java/LinuxClientDemoV4.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author winson
@@ -9,30 +10,42 @@ import java.net.Socket;
 public class LinuxClientDemoV4 {
 
     public static void main(String[] args) {
-
-        Socket socket = new Socket();
+        CountDownLatch latch = new CountDownLatch(1);
         try {
-            socket.bind(new InetSocketAddress("192.168.159.1", 10005));
-
-            socket.connect(new InetSocketAddress("192.168.159.130", 20001));
-            System.out.println("connect server success ... ");
-            System.in.read();
-            while (true){
-                byte[] buf = new byte[1024];
-                int readLength = System.in.read(buf);
-                for (int i = 0; i < readLength; i++) {
-                    socket.getOutputStream().write(buf[i]);
+            for (int i = 10001; i < 65525; i++) {
+                try {
+                    Socket socket = new Socket();
+                    socket.bind(new InetSocketAddress("192.168.159.1", i));
+                    socket.connect(new InetSocketAddress("192.168.159.130", 10000));
+                }catch (Exception e){
+                    e.printStackTrace();
                 }
-            }
 
-        } catch (IOException e) {
+            }
+//            System.out.println("connect server success ... ");
+//            System.in.read();
+//            while (true){
+//                byte[] buf = new byte[1024];
+//                int readLength = System.in.read(buf);
+//                for (int i = 0; i < readLength; i++) {
+//                    socket.getOutputStream().write(buf[i]);
+//                }
+//            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+//            try {
+//                socket.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         System.out.println("client demo finish ");
---
+++ src/main/java/LinuxServerDemoV2.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.ServerSocketChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author winson
+ * @date 2021/8/23
+ **/
+public class LinuxServerDemoV2 {
+
+    public static void main(String[] args) {
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        try {
+//            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+//            serverSocketChannel.configureBlocking(false);
+//            serverSocketChannel.accept();
+
+            ServerSocket serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress("192.168.195.1", 10000));
+
+            List<Socket> socketList = new ArrayList<>();
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                socketList.add(socket);
+                System.out.println("accept socket size : " + socketList.size());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("server finish ... ");
+    }
+
+}
