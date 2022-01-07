4813f2fe7e15dd272bddd59028eda06f272795fe
zhouwenxiang <929566854@qq.com>
2022-01-07 11:54:26 +0800

    代码测试

--- java-api/src/main/java/com/winson/utils/regular/RegularDemoV7.java
+++ java-api/src/main/java/com/winson/utils/regular/RegularDemoV7.java
@@ -6,11 +6,94 @@ package com.winson.utils.regular;
  **/
 public class RegularDemoV7 {
 
+    public static final String[] ss = {
+            "北京大学",
+            "中国人民大学",
+            "清华大学",
+            "北京航空航天大学",
+            "北京理工大学",
+            "北京邮电大学",
+            "北京交通大学",
+            "复旦大学",
+            "上海交通大学",
+            "同济大学",
+            "上海理工大学",
+            "华东师范大学",
+            "上海财经大学",
+            "上海外国语大学",
+            "华东理工大学",
+            "东华大学",
+            "上海大学",
+            "华南理工大学",
+            "中山大学",
+            "暨南大学",
+            "广东财经大学",
+            "广东工业大学",
+            "深圳大学",
+            "南方科技大学",
+            "香港中文大学（深圳）",
+            "北京大学深圳研究生院",
+            "清华大学深圳研究生院",
+            "哈尔滨工业大学深圳研究生院",
+            "南京理工大学",
+            "河海大学",
+            "东南大学",
+            "南京邮电大学",
+            "南京大学",
+            "浙江大学",
+            "湘潭大学",
+            "湖南师范大学",
+            "中南大学",
+            "湖南大学",
+            "电子科技大学",
+            "四川大学",
+            "西南财经大学",
+            "西南交通大学",
+            "中南财经政法大学",
+            "华中师范大学",
+            "武汉理工大学",
+            "中国地质大学（武汉）",
+            "华中农业大学",
+            "华中科技大学",
+            "武汉大学",
+            "中南民族大学",
+            "西安电子科技大学",
+            "长安大学",
+            "西安交通大学",
+            "西安理工大学",
+            "哈尔滨工业大学",
+            "哈尔滨工程大学",
+            "吉林大学",
+            "大连理工大学",
+            "东北大学",
+            "河北工业大学",
+            "天津大学",
+            "南开大学",
+            "中国科学技术大学",
+            "合肥工业大学",
+            "安徽大学",
+            "江南大学",
+            "南昌大学",
+            "江西财经大学",
+            "山东大学"
+    };
+
     public static void main(String[] args) {
 
-        String reg = ".+";
-        String source = " 我们 是 t";
-        System.out.println(source.matches(reg));
+//        String reg = "(^香港中文大学$)|(^香港中文大学（深圳）$)";
+//        String reg2 = "香港中文大学";
+//
+//        String target = "香港中文大学（深圳）";
+//        System.out.println(target.matches(reg));
+//        System.out.println(target.matches(reg2));
+//        StringBuilder sb = new StringBuilder();
+//        for (String s : ss) {
+////            System.out.println(s);
+//            sb.append("(^" +s + "$)|");
+//        }
+//        System.out.println(sb);
+
+        System.out.println(System.currentTimeMillis());
 
     }
 
