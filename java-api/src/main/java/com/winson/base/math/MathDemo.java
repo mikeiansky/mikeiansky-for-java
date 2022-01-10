a994167718e175569842cbe6a585b709c4e30411
zhouwenxiang <929566854@qq.com>
2022-01-10 17:04:05 +0800

    新增数据计算测试类

---
+++ java-api/src/main/java/com/winson/base/math/MathDemo.java
@@ -0,0 +1,18 @@
+package com.winson.base.math;
+
+/**
+ * @author winson
+ * @date 2022/1/10
+ **/
+public class MathDemo {
+
+    public static void main(String[] args) {
+
+
+        long s = System.currentTimeMillis() / 1000;
+        double div = 10000000000.0;
+        System.out.println(s/div);
+
+    }
+
+}
