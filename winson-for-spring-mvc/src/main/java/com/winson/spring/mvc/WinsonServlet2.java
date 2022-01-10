d4ce01066ab89a6f83ee08c4f28e8b06526ddafb
zhouwenxiang <929566854@qq.com>
2022-01-10 18:42:10 +0800

    servlet测试

--- winson-for-spring-mvc/src/main/java/com/winson/spring/mvc/WinsonServlet2.java
+++ winson-for-spring-mvc/src/main/java/com/winson/spring/mvc/WinsonServlet2.java
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
  * @author winson
  * @date 2022/1/4
  **/
-public class WinsonServlet extends HttpServlet {
+public class WinsonServlet2 extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -19,7 +19,7 @@ public class WinsonServlet extends HttpServlet {
         System.out.println("winson servlet do get ...... ");
         resp.setContentType("text/html;charset=UTF-8");
         PrintWriter writer = resp.getWriter();
-        writer.write("winson --- C语言中文网，c.biancheng.net");
+        writer.write("winson 6666 --- C语言中文网，c.biancheng.net");
         writer.close();
     }
 
