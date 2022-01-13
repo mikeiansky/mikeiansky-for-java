6fabd37caceb39b2f204acab9b261bac7ff31314
zhouwenxiang <929566854@qq.com>
2022-01-13 09:52:16 +0800

    项目工程结构修改

--- java-api/src/main/java/com/winson/base/math/MathDemo.java
+++ java-api/src/main/java/com/winson/base/math/MathDemo.java
@@ -1,5 +1,7 @@
 package com.winson.base.math;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author winson
  * @date 2022/1/10
@@ -12,6 +14,13 @@ public class MathDemo {
         long s = System.currentTimeMillis() / 1000;
         double div = 10000000000.0;
         System.out.println(s/div);
+
+        AtomicInteger ai = new AtomicInteger();
+        ai.set(Integer.MAX_VALUE);
+        System.out.println(ai.get());
+        System.out.println(ai.getAndIncrement());
+        System.out.println(ai.getAndIncrement());
+        System.out.println(ai.getAndIncrement());
 
     }
 
