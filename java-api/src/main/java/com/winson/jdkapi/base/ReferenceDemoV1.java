bee6528524b558aeb2ac68ebb4b9db7375cf1a01
zhouwenxiang <929566854@qq.com>
2022-02-18 16:10:59 +0800

    测试字符串处理

--- java-api/src/main/java/com/winson/jdkapi/base/ReferenceDemoV1.java
+++ java-api/src/main/java/com/winson/jdkapi/base/ReferenceDemoV1.java
@@ -5,4 +5,13 @@ package com.winson.jdkapi.base;
  * @date 2021/10/16
  **/
 public class ReferenceDemoV1 {
+
+    public static void main(String[] args) {
+
+        String name = "周文翔";
+        System.out.println(name.substring(1,name.length()));
+        System.out.println(name.substring(0,1));
+
+    }
+
 }
