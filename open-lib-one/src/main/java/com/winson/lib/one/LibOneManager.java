e4c6f079a07381cd5fce481b28dffedcf5c25816
zhouwenxiang <929566854@qq.com>
2022-04-20 09:25:30 +0800

    maven的依赖和class的加载时机测试

---
+++ open-lib-one/src/main/java/com/winson/lib/one/LibOneManager.java
@@ -0,0 +1,13 @@
+package com.winson.lib.one;
+
+/**
+ * @author winson
+ * @date 2022/4/20
+ **/
+public class LibOneManager {
+
+    public void say(String msg) {
+        System.out.println("LibOneManager say msg : " + msg);
+    }
+
+}
