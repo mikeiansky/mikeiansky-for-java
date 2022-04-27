7e80422d02099a6afb14297f66e8a51fbf3e4e23
zhouwenxiang <929566854@qq.com>
2022-04-27 17:42:46 +0800

    加载测试

--- java-api/src/main/java/com/winson/jdkapi/common/DataRangeDemoV1.java
+++ java-api/src/main/java/com/winson/jdkapi/common/DataRangeDemoV1.java
@@ -8,36 +8,39 @@ public class DataRangeDemoV1 {
 
     public static void main(String[] args) {
 
-        System.out.println(Float.MAX_VALUE);
-        System.out.println(Float.MIN_NORMAL);
-        System.out.println(Float.MIN_VALUE);
-        System.out.println(Float.MIN_EXPONENT);
+//        System.out.println(Float.MAX_VALUE);
+//        System.out.println(Float.MIN_NORMAL);
+//        System.out.println(Float.MIN_VALUE);
+//        System.out.println(Float.MIN_EXPONENT);
+//
+////        float f1 = -100.0f;
+////        float f2 = -200.0f;
+////        float f3 = f1 + f2;
+////        System.out.println(f3);
+////        System.out.println(Float.MIN_VALUE > f3);
+//
+//        int maxV = Integer.MAX_VALUE;
+//        int minV = Integer.MIN_VALUE;
+//        System.out.println(maxV);
+//        System.out.println(minV);
+////        int ov = maxV + 1;
+////        System.out.println(ov);
+//
+//        Float mf = -Float.MAX_VALUE;
+//        System.out.println(mf);
+//        Float of = mf + 1000000000000000000000f;
+//        System.out.println(of);
+//        float ff1 = 16777215;
+//        System.out.println(ff1);
+//        float ff2 = 16777216 * 2;
+//        System.out.println(ff2);
+////        System.out.println(2 ^ 25);
+//        System.out.println(Double.MIN_VALUE);
+//        System.out.println(Double.MAX_VALUE);
 
-//        float f1 = -100.0f;
-//        float f2 = -200.0f;
-//        float f3 = f1 + f2;
-//        System.out.println(f3);
-//        System.out.println(Float.MIN_VALUE > f3);
-
-        int maxV = Integer.MAX_VALUE;
-        int minV = Integer.MIN_VALUE;
-        System.out.println(maxV);
-        System.out.println(minV);
-//        int ov = maxV + 1;
-//        System.out.println(ov);
-
-        Float mf = -Float.MAX_VALUE;
-        System.out.println(mf);
-        Float of = mf + 1000000000000000000000f;
-        System.out.println(of);
-        float ff1 = 16777215;
-        System.out.println(ff1);
-        float ff2 = 16777216 * 2;
-        System.out.println(ff2);
-//        System.out.println(2 ^ 25);
-        System.out.println(Double.MIN_VALUE);
-        System.out.println(Double.MAX_VALUE);
-
+        String date = "2022-01-02 01:02:03";
+        System.out.println(date);
+        System.out.println(date.substring(0,10));
 
     }
 
