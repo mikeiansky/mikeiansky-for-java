c8daa02bbd26d6155a30fa5e0bd96008877453ec
zhouwenxiang <929566854@qq.com>
2022-05-13 11:42:53 +0800

    引用与回收测试

--- java-api/src/main/java/com/winson/reference/SoftReferenceDemoV1.java
+++ java-api/src/main/java/com/winson/reference/SoftReferenceDemoV1.java
@@ -15,17 +15,25 @@ public class SoftReferenceDemoV1 {
         System.out.println("soft reference demo v1 start ");
         System.out.println("one");
         byte[] buf1 = new byte[1024*1024];
-        SoftReference<byte[]> s1 = new SoftReference<>(new byte[1024*512]);
+//        byte[] buff = new byte[1024*1024];
+//        SoftReference<byte[]> s1 = new SoftReference<>(new byte[1024*512]);
+        SoftReference<byte[]> s1 = new SoftReference<>(buf1);
+        buf1 = null;
         System.out.println("after one:"+s1.get());
         System.out.println("two");
         byte[] buf2 = new byte[1024*1024*5];
         System.out.println("after two:"+s1.get());
+        byte[] buf3 = new byte[1024*1024];
         System.out.println(s1);
+        System.out.println("after three:"+s1.get());
+        byte[] buf4 = new byte[1024*1024];
+        System.out.println("after four:"+s1.get());
+//        byte[] buf5 = new byte[1024*1024];
 //        System.out.println(System.getProperty("FileDir"));
         System.out.println("soft reference demo v1 end ");
 
-        System.out.println(VM.getSavedProperty("java.lang.Integer.IntegerCache.high"));
-        System.out.println(VM.getSavedProperty("-XX:+UseSerialGC"));
+//        System.out.println(VM.getSavedProperty("java.lang.Integer.IntegerCache.high"));
+//        System.out.println(VM.getSavedProperty("-XX:+UseSerialGC"));
     }
 
 }
--- java-api/src/main/java/com/winson/reference/WeakReferenceDemoV1.java
+++ java-api/src/main/java/com/winson/reference/WeakReferenceDemoV1.java
@@ -1,6 +1,7 @@
 package com.winson.reference;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 /**
  * @author winson
@@ -11,15 +12,34 @@ public class WeakReferenceDemoV1 {
     //-XX:+UseSerialGC -XX:+PrintGCDetails -Xmx10m -Xms2m -Xmn2m
     public static void main(String[] args) {
         System.out.println("weak reference demo start");
-
+        ArrayList<byte[]> record = new ArrayList<>();
         byte[] buf1 = new byte[1024 * 1024];
         System.out.println("one");
-        WeakReference<byte[]> wr = new WeakReference<>(new byte[1024* 1024]);
+        // 方式1
+//        WeakReference<byte[]> wr = new WeakReference<>(new byte[1024* 1024]);
+        WeakReference<byte[]> wr = new WeakReference<>(buf1);
+        System.out.println("buf1 : " + buf1);
+        buf1 = null;
         System.out.println("wr.get after one : "+wr.get());
         byte[] buf2 = new byte[1024*1024];
+        record.add(buf2);
         System.out.println("wr.get after two : "+wr.get());
         byte[] buf3 = new byte[1024*1024];
+        record.add(buf3);
         System.out.println("wr.get after three : "+wr.get());
+        byte[] buf4 = new byte[1024*1024];
+        record.add(buf4);
+        System.out.println("wr.get after four : "+wr.get());
+        byte[] buf5 = new byte[1024*1024];
+        record.add(buf5);
+        System.out.println("wr.get after five : "+wr.get());
+        byte[] buf6 = new byte[1024*1024];
+        record.add(buf6);
+        byte[] buf7 = new byte[1024*1024];
+        record.add(buf7);
+        byte[] buf8 = new byte[1024*1024];
+        record.add(buf8);
+        System.out.println("wr.get after end : "+wr.get());
 
         System.out.println("weak reference demo end");
     }
