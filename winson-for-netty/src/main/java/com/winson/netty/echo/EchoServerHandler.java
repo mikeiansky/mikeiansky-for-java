b78d12eef8d6dcc7e597212a11b7e16e0045aa2b
winson <929566854@qq.com>
2022-05-21 13:56:19 +0800

    新增Netty测试项目

---
+++ winson-for-netty/src/main/java/com/winson/netty/echo/EchoServerHandler.java
@@ -0,0 +1,38 @@
+package com.winson.netty.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+
+/**
+ * @author winson
+ * @date 2022/5/21
+ **/
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+//        ctx.write("hello client".getBytes(StandardCharsets.UTF_8));
+//        System.out.println("msg:class:" + msg.getClass());
+//        System.out.println("channelRead msg:" + msg);
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println("server receive message : " + byteBuf.toString(Charset.defaultCharset()));
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ winson-for-netty/src/main/java/com/winson/netty/echo/NettyEchoServerDemoV1.java
@@ -0,0 +1,47 @@
+package com.winson.netty.echo;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author winson
+ * @date 2022/5/21
+ **/
+public class NettyEchoServerDemoV1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 100)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(serverHandler);
+                    }
+                });
+
+        ChannelFuture cf = b.bind(8007).sync();
+        cf.channel().closeFuture().sync();
+
+        bossGroup.shutdownGracefully();
+        workGroup.shutdownGracefully();
+
+    }
+
+}
