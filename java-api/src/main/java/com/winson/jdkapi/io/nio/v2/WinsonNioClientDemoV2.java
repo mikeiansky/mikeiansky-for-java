bed2b7a60c54c0333847414b69cc97268b3b2e59
zhouwenxiang <929566854@qq.com>
2022-06-07 19:10:13 +0800

    测试selector

--- java-api/src/main/java/com/winson/jdkapi/io/nio/v2/WinsonNioClientDemoV2.java
+++ java-api/src/main/java/com/winson/jdkapi/io/nio/v2/WinsonNioClientDemoV2.java
@@ -3,6 +3,8 @@ package com.winson.jdkapi.io.nio.v2;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.spi.SelectorProvider;
 
 /**
  * @author winson
@@ -11,11 +13,15 @@ import java.net.Socket;
 public class WinsonNioClientDemoV2 {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        Socket socket = new Socket();
-        socket.connect(new InetSocketAddress(20001));
-        socket.close();
-        System.out.println("v2 client execute complete ... ");
-        Thread.sleep(5000);
+//        Socket socket = new Socket();
+//        socket.connect(new InetSocketAddress(20001));
+//        socket.close();
+//        System.out.println("v2 client execute complete ... ");
+//        Thread.sleep(5000);
+
+        ServerSocketChannel channel = SelectorProvider.provider().openServerSocketChannel();
+        System.out.println(channel.isOpen());
+
     }
 
 }
