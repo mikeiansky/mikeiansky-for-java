14c47b8f2b2f8b55077c8ce1af7445e70be336d6
zhouwenxiang <929566854@qq.com>
2022-06-30 18:30:51 +0800

    父工程定义maven插件

--- winson-for-maven-plugin/src/main/java/com/winson/maven/plugin/InfoMojo.java
+++ winson-for-maven-plugin/src/main/java/com/winson/maven/plugin/InfoMojo.java
@@ -3,13 +3,21 @@ package com.winson.maven.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
 /**
  * @author winson
  * @date 2022/6/30
  **/
-@Mojo(name = "info")
+@Mojo(name = "info",
+        defaultPhase = LifecyclePhase.PACKAGE,
+//        requiresProject = true,
+        threadSafe = true,
+        requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME,
+        requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME
+)
 public class InfoMojo extends AbstractMojo {
 
     @Override
