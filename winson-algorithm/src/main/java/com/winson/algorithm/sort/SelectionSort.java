021d4e80bbcb13085ece04789953525e0dd78574
mikeian <929566854@qq.com>
2023-08-17 10:15:45 +0800

    优化排序算法

--- winson-algorithm/src/main/java/com/winson/algorithm/sort/SelectionSort.java
+++ winson-algorithm/src/main/java/com/winson/algorithm/sort/SelectionSort.java
@@ -8,23 +8,23 @@ package com.winson.algorithm.sort;
 public class SelectionSort {
 
     public static void selectionSort(int data[]) {
-        for (int i = 1; i < data.length - 1; i++) {
+        for (int i = 0; i < data.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < data.length; j++) {
                 if (data[minIndex] > data[j]) {
                     minIndex = j;
                 }
             }
-//            if (data[i - 1] > data[minIndex]) {
-                int temp = data[i - 1];
-                data[i - 1] = data[minIndex];
-                data[minIndex] = temp;
-//            }
+            if (minIndex != i) {
+                int temp = data[minIndex];
+                data[minIndex] = data[i];
+                data[i] = temp;
+            }
         }
     }
 
     public static void main(String[] args) {
-        int data[] = {3, 1, 6, 8, 9, 2, 5, 7};
+        int data[] = {1, 3, 6, 8, 9, 2, 5, 7};
         selectionSort(data);
         for (int i = 0; i < data.length; i++) {
             System.out.println(data[i]);
