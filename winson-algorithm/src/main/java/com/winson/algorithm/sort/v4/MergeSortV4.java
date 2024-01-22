0fbc387a1c73fc7e6473b81218b90b23089caf9c
mikeian <929566854@qq.com>
2024-01-22 10:27:29 +0800

    排序算法学习

--- winson-algorithm/src/main/java/com/winson/algorithm/sort/v4/MergeSortV4.java
+++ winson-algorithm/src/main/java/com/winson/algorithm/sort/v4/MergeSortV4.java
@@ -2,71 +2,13 @@ package com.winson.algorithm.sort.v4;
 
 /**
  * @author mike ian
- * @date 2024/1/19
- * @desc 归并排序
+ * @date 2024/1/22
+ * @desc 快速排序v4版本
  **/
 public class MergeSortV4 {
 
-    /**
-     * 归并排序思想，递归方式，将数据进行拆分，排序，然后在合并，有些问题，后面再看
-     *
-     * @param data
-     */
-    public static void sort(int[] data, int start, int end) {
-        int length = end - start + 1;
-        int[] temp = new int[length];
-        mergerSort(data, temp, start, end);
-    }
-
-    public static void mergerSort(int[] data, int[] temp, int start, int end) {
-        int length = end - start + 1;
-        if (length == 0) {
-            return;
-        }
-        if (length == 1) {
-            temp[start] = data[start];
-            return;
-        }
-        if (length == 2) {
-            if (data[start] > data[end]) {
-                temp[start] = data[end];
-                temp[end] = data[start];
-            } else {
-                temp[start] = data[start];
-                temp[end] = data[end];
-            }
-            return;
-        }
-
-        int mid = start + length / 2;
-        mergerSort(data, temp, start, mid);
-        mergerSort(data, temp, mid + 1, end);
-
-        // 合并
-        int i = start;
-        int j = mid;
-        for (int k = start; k <= end; k++) {
-            if (i > mid) {
-                data[k] = temp[j];
-                j++;
-            } else if (j >= end) {
-                data[k] = temp[i];
-                i++;
-            } else {
-                if (temp[i] < temp[j]) {
-                    data[k] = temp[i];
-                    i++;
-                } else {
-                    data[k] = temp[j];
-                    j++;
-                }
-            }
-        }
-
-    }
 
     public static void main(String[] args) {
-
         int data[] = {26, 11, 56, 16, 30, 29, 99, 64};
 
         System.out.println("排序前");
@@ -75,14 +17,67 @@ public class MergeSortV4 {
             System.out.print(',');
         }
 
-        sort(data, 0, data.length - 1);
+        data = mergeSort(data, 0, data.length - 1);
 
+        System.out.println();
         System.out.println("排序后");
         for (int datum : data) {
             System.out.print(datum);
             System.out.print(',');
         }
 
+    }
+
+    private static int[] mergeSort(int[] data, int start, int end) {
+        int length = end - start + 1;
+        int[] temp = new int[length];
+        // 先拆分，按二分进行拆分
+        if (length <= 1) {
+            temp[0] = data[start];
+            return temp;
+        }
+        if (length == 2) {
+            if (data[start] <= data[end]) {
+                temp[0] = data[start];
+                temp[1] = data[end];
+            } else {
+                temp[0] = data[end];
+                temp[1] = data[start];
+            }
+            return temp;
+        }
+
+        int mid = start + length / 2;
+
+        // 拆分
+        int[] head = mergeSort(data, start, mid);
+        int[] tail = mergeSort(data, mid + 1, end);
+
+        int headLength = head.length;
+        int tailLength = tail.length;
+
+        // 排序
+        int headIndex = 0;
+        int tailIndex = 0;
+        for (int i = 0; i < length; i++) {
+            if (headIndex >= headLength) {
+                temp[i] = tail[tailIndex];
+                tailIndex++;
+            } else if (tailIndex >= tailLength) {
+                temp[i] = head[headIndex];
+                headIndex++;
+            } else {
+                if (head[headIndex] <= tail[tailIndex]) {
+                    temp[i] = head[headIndex];
+                    headIndex++;
+                } else {
+                    temp[i] = tail[tailIndex];
+                    tailIndex++;
+                }
+            }
+        }
+
+        return temp;
     }
 
 }
