e6531bacbd41b13dd995bed9b9f3371c3d387671
mikeian <929566854@qq.com>
2024-04-08 16:26:20 +0800

    zookeeper 集群测试

--- winson-for-spring-boot-02/src/main/java/com/winson/springboot02/ZKApp.java
+++ winson-for-spring-boot-02/src/main/java/com/winson/springboot02/ZKApp.java
@@ -1,10 +1,12 @@
 package com.winson.springboot02;
 
 import jakarta.annotation.PostConstruct;
+import org.I0Itec.zkclient.IZkDataListener;
 import org.apache.zookeeper.*;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -17,7 +19,12 @@ public class ZKApp {
 
     public void connectZKServer() throws Exception {
         ZooKeeper zooKeeper = new ZooKeeper(
-                "172.16.2.241:2181",
+                "172.16.2.241:2181," +
+                        "172.16.2.242:2181," +
+                        "172.16.2.243:2181," +
+                        "172.16.2.244:2181," +
+                        "172.16.2.245:2181," +
+                        "172.16.2.246:2181",
                 20000,
                 new Watcher() {
                     @Override
@@ -49,29 +56,53 @@ public class ZKApp {
 //        System.out.println(new String(data));
 //        System.out.println("connect ----> 2");
 
-        zooKeeper.addWatch("/app", new Watcher() {
+        zooKeeper.addWatch("/temp", new Watcher() {
             @Override
             public void process(WatchedEvent event) {
+                event.getType();
                 System.out.println("node app :: " + event);
                 System.out.println("watch node path :: " + event.getPath());
+
                 try {
-                    System.out.println("new data :: " +
-                            new String(zooKeeper.getData(event.getPath(), false, null)));
-                } catch (Exception e) {
+                    List<String> children = zooKeeper.getChildren("/temp", false);
+                    System.out.println("before sort : " + children);
+                    children.sort(Comparator.naturalOrder());
+                    System.out.println("after sort : " + children);
+                } catch (KeeperException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+
+//                try {
+//                    zooKeeper.removeWatches("/temp", this, WatcherType.Persistent, false);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                } catch (KeeperException e) {
+//                    throw new RuntimeException(e);
+//                }
+
+//                try {
+//                    System.out.println("new data :: " +
+//                            new String(zooKeeper.getData(event.getPath(), false, null)));
+//                } catch (Exception e) {
+//                    throw new RuntimeException(e);
+//                }
             }
         }, AddWatchMode.PERSISTENT);
 
         System.out.println("connect ----> 3");
 
-        String lockValue = "lock-002";
-        zooKeeper.create("/lock", lockValue.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+        String lockValue = "lock-004";
+//        String result = zooKeeper.create("/temp/", lockValue.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        String result = zooKeeper.create("/temp/", lockValue.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("create lock success : " + result);
 
-        List<String> lockChildren = zooKeeper.getChildren("/lock", false);
-        System.out.println("lockChildren :: " + lockChildren);
+//        List<String> lockChildren = zooKeeper.getChildren("/lock", false);
+//        System.out.println("lockChildren :: " + lockChildren);
+//        System.out.println("create lock success");
 
-        System.out.println("create lock success");
+
 
         boolean flag = true;
         while (flag) {
