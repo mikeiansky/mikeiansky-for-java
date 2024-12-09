6b95b410ce5e626c212d136ac9f40dc7da8e519e
mikeian <zhouwenxiang@ciwei.net>
2024-12-09 16:44:24 +0800

    test the initialization sequence of java object

---
+++ mikeiansky-for-java-base/src/main/java/io/github/mikeiansky/java/base/jdk/base/InitializationSequenceDemo.java
@@ -0,0 +1,26 @@
+package io.github.mikeiansky.java.base.jdk.base;
+
+/**
+ * @author mike ian
+ * @date 2024/12/9
+ * @desc
+ **/
+public class InitializationSequenceDemo {
+
+    public static class Inner {
+        public Inner(){
+            System.out.println("Inner created");
+        }
+    }
+
+    private Inner inner = new Inner();
+
+    public InitializationSequenceDemo(){
+        System.out.println("InitializationSequenceDemo created");
+    }
+
+    public static void main(String[] args) {
+        InitializationSequenceDemo demo = new InitializationSequenceDemo();
+    }
+
+}
