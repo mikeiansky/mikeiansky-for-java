e132919c3b85d7c05e502001d41851f062a6003d
ian <929566854@qq.com>
2025-06-07 21:34:07 +0800

    Test call function by class or object;

--- mikeiansky-for-java-base/src/main/java/io/github/mikeiansky/java/base/jdk/func/ClassMethodCallDemo.java
+++ mikeiansky-for-java-base/src/main/java/io/github/mikeiansky/java/base/jdk/func/ClassMethodCallDemo.java
@@ -1,5 +1,7 @@
 package io.github.mikeiansky.java.base.jdk.func;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
@@ -32,9 +34,22 @@ public class ClassMethodCallDemo {
 
     }
 
+    public interface ThreeArgFunc<A1, A2, A3> {
+        void useThree(A1 a1, A2 a2, A3 a3);
+    }
+
     public static class CallFactory {
         public static void doClazzCall(Supplier<One> supplier, BiFunction<HelloCall, String, String> call, String name) {
             call.apply(supplier.get(), name);
+        }
+
+        public static <K, V> Map<K, V> useMap(Supplier<Map<K, V>> supplier,
+                                              ThreeArgFunc<Map<K, V>, K, V> threeArgFunc,
+                                              K key, V value) {
+            System.out.println("Using map with key: " + key + ", value: " + value);
+            Map<K, V> map = supplier.get();
+            threeArgFunc.useThree(map, key, value);
+            return map;
         }
     }
 
@@ -43,6 +58,8 @@ public class ClassMethodCallDemo {
         CallFactory.doClazzCall(One::new, HelloCall::change, "phone");
         CallFactory.doClazzCall(One::new, HelloCall::call, "ian");
         CallFactory.doClazzCall(One::new, HelloCall::change, "pad");
+        Map<Integer,String> result = CallFactory.useMap(HashMap::new, Map::put, 1, "one");
+        System.out.println("Resulting map: " + result);
     }
 
 }
