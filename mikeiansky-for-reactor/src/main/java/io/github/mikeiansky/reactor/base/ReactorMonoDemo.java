ab84825c73b52b358e770c0b0e8d1a56bc18cd3d
ian <929566854@qq.com>
2025-06-08 17:20:33 +0800

    Test reactor core flux and mono;

--- mikeiansky-for-reactor/src/main/java/io/github/mikeiansky/reactor/base/ReactorMonoDemo.java
+++ mikeiansky-for-reactor/src/main/java/io/github/mikeiansky/reactor/base/ReactorMonoDemo.java
@@ -2,6 +2,8 @@ package io.github.mikeiansky.reactor.base;
 
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 /**
  * @author mike ian
  * @date 2025/6/8
@@ -9,11 +11,17 @@ import reactor.core.publisher.Mono;
  **/
 public class ReactorMonoDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Mono<String> mono = Mono.just("Hello, Reactor Mono!");
         mono.subscribe(System.out::println);
 
+        System.out.println("before mono delay");
+        Mono.delay(Duration.ofSeconds(2))
+                .then(Mono.just("hello after delay"))
+                .subscribe(System.out::println) ;
+
+        Thread.sleep(3000);
     }
 
 }
