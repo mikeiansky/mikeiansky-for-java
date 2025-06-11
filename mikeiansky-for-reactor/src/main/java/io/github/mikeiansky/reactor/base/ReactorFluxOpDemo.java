eec15cfc4ed324001e9f49f3d686115e304c84c7
mikeian <zhouwenxiang@ciwei.net>
2025-06-11 16:06:16 +0800

    Test reactor operation;

--- mikeiansky-for-reactor/src/main/java/io/github/mikeiansky/reactor/base/ReactorFluxOpDemo.java
+++ mikeiansky-for-reactor/src/main/java/io/github/mikeiansky/reactor/base/ReactorFluxOpDemo.java
@@ -1,6 +1,10 @@
 package io.github.mikeiansky.reactor.base;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.function.Function;
 
 /**
  *
@@ -15,6 +19,9 @@ public class ReactorFluxOpDemo {
         Flux.just(1, 2, 3, 4, 5)
                 .map(i -> i * 2) // Multiply each element by 2
                 .filter(i -> i > 5) // Filter elements greater than 5
+                .collectList()
+//                .flatMap(integers -> null)
+//                .handle((k, v) -> {})
                 .subscribe(System.out::println); // Print the resulting elements
 
     }
