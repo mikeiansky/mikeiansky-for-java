c61e43476e19bfe1af2e48826ab5d9188cff25f3
mikeian <zhouwenxiang@xiaofupay.com>
2025-09-01 15:15:24 +0800

    add grpc for java implement;

---
+++ mikeiansky-for-java-base/src/main/java/io/github/mikeiansky/grpc/HelloServiceImpl.java
@@ -0,0 +1,15 @@
+package io.github.mikeiansky.grpc;
+
+import com.example.grpc.HelloRequest;
+import com.example.grpc.HelloResponse;
+import com.example.grpc.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        super.sayHello(request, responseObserver);
+    }
+
+}
