a54ee29afad724f152cc26115b531ef2d772af96
mikeian <zhouwenxiang@xiaofupay.com>
2025-09-01 15:48:56 +0800

    Test grpc server and client demo;

--- mikeiansky-for-java-base/src/main/java/io/github/mikeiansky/grpc/HelloServiceImpl.java
+++ mikeiansky-for-java-base/src/main/java/io/github/mikeiansky/grpc/HelloServiceImpl.java
@@ -7,6 +7,8 @@ import io.grpc.stub.StreamObserver;
 
 public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
 
+
+
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
         super.sayHello(request, responseObserver);
---
+++ mikeiansky-for-java-base/src/main/java/io/github/mikeiansky/grpc/RpcClientDemo.java
@@ -0,0 +1,42 @@
+package io.github.mikeiansky.grpc;
+
+import com.example.grpc.HelloRequest;
+import com.example.grpc.HelloResponse;
+import com.example.grpc.HelloServiceGrpc;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+
+public class RpcClientDemo {
+
+        //使用main方法来测试client端
+        public static void main(String[] args) throws Exception {
+            RpcClientDemo clientDemo = new RpcClientDemo();
+            try {
+                //基于gRPC远程调用对应的方法
+                clientDemo.remoteCall("【mikeian】");
+            } finally {
+            }
+        }
+        /**
+         * 基于gRPC框架的使用步骤，进行远程调用
+         * @param name
+         */
+        public void remoteCall(String name) {
+            HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+            HelloResponse response;
+            try {
+                // 基于访问地址 创建通道
+                Channel channel =  ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+                // 利用通道 创建一个桩（Stub）对象
+                HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(channel);
+                //通过桩对象来调用远程方法
+                response = blockingStub.sayHello(request);
+            } catch (StatusRuntimeException e) {
+                return;
+            }
+            System.out.println("client端远程调用sayHello()的结果为：\n\n" + response.getMessage());
+        }
+
+}
